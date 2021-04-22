ab7c4538049289afaa4631433e78d6c8c5adc87b
KlaasHagge <klaash@ppedv.de>
2021-04-22 11:59:01 +0200

    Validation

---
+++ Templates/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Templates
+{
+    //Bsp-Klasse
+    public class Person
+    {
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+        public int Alter { get; set; }
+
+        public override string ToString()
+        {
+            return Nachname;
+        }
+    }
+}
---
+++ Validation/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace Validation
+{
+    public class Person : IDataErrorInfo
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value.All(x => Char.IsLetter(x)))
+                    name = value;
+                else
+                    throw new Exception("Bitte gib nur Buchstaben ein");
+            }
+        }
+
+        public int Alter { get; set; }
+
+
+        public string Error => null;
+
+        public string this[string columnName]
+        {
+            get
+            {
+                switch (columnName)
+                {
+                    case (nameof(Alter)):
+                        if (Alter < 0) return "Bitte gib dein wahres Alter an.";
+                        if (Alter > 150) return "Zu Alt";
+                        break;
+                    default:
+                        break;
+                }
+
+                return null;
+            }
+        }
+    }
+}
