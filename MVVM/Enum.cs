eeb920a689a63ef63d0de87e23e2b23a27901348
KlaasHagge <klaash@ppedv.de>
2021-04-23 12:12:49 +0200

    Commands, MVVM

---
+++ MVVM/Enum.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVVM
+{
+    public enum Gender { Männlich, Weiblich, Divers }
+}
---
+++ MVVM/ViewModel/StartViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows;
+
+namespace MVVM.ViewModel
+{
+    public class StartViewModel : INotifyPropertyChanged
+    {
+        public CustomCommand LadeDbCmd { get; set; }
+        public CustomCommand OeffneDbCmd { get; set; }
+
+        public int AnzahlPersonen { get { return Model.Person.Personenliste.Count; } }
+
+        public StartViewModel()
+        {
+            LadeDbCmd = new CustomCommand
+                (
+                    p =>
+                    {
+                        Model.Person.LadePersonenAusDb();
+
+                        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(AnzahlPersonen)));
+                    },
+                    p => AnzahlPersonen == 0
+                );
+
+            OeffneDbCmd = new CustomCommand
+                (
+                    p =>
+                    {
+                        //TODO: Neues Fenster öffnen
+
+                        (p as Window).Close();
+                    },
+                    p => AnzahlPersonen > 0
+                );
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
