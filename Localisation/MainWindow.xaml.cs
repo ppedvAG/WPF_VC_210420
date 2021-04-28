4887afcdb38ce3daa2918989ada8cafc3707f09d
KlaasHagge <klaash@ppedv.de>
2021-04-28 15:21:41 +0200

    final (kommentiert)

--- Localisation/MainWindow.xaml.cs
+++ Localisation/MainWindow.xaml.cs
@@ -28,15 +28,18 @@ namespace Localisation
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            //Testen der aktuellen Sprache
             if (Thread.CurrentThread.CurrentUICulture.Name == "en-US")
+                //Ändern der Sprache
                 Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("de-DE");
             else
                 Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en-US");
 
+            //Neu-Öffnen des aktuellen Fensters (mit neuer Sprache)
             new MainWindow().Show();
 
+            //Schließen des alten Fensters
             this.Close();
-
         }
     }
 }
